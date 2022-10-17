8a9cbf7cd57c6fcd4a8706d8537cf99275919369
wanle <wanle97@163.com>
2022-10-18 00:34:57 +0800

    hot-fix1.0 第一次提交

---
+++ src/main/java/Test02.java
@@ -0,0 +1,8 @@
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+import org.w3c.dom.ls.LSOutput;
+
+public class Test02 {
+    public static void main(String[] args) {
+        System.out.println("hot-fix1.0");
+    }
+}
